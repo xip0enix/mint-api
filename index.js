b8315ad45d2fe57f54a7b3773643f2764b23dbca
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-05-05 16:42:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const PORT = process.env.PORT|| 7000
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const PORT = process.env.PORT || 7000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const app = express()
+const app = express();
 
-const articles = []
+app.get("/", (req, res) => {
+  res.json("Welcome to my MINT-EC API");
+});
 
-app.get('/', (req, res) => {
-    res.json('Welcome to my MINT-EC API')
-})
+app.get("/api/veranstaltungen", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://www.mint-ec.de/angebote/veranstaltungen/kommende-veranstaltungen/"
+    );
 
-app.get('/api/veranstaltungen', (req, res) => {
-    axios.get('https://www.mint-ec.de/angebote/veranstaltungen/kommende-veranstaltungen/')
-    .then((response) => {
-        const html = response.data
-        const $ = cheerio.load(html)
+    const $ = cheerio.load(response.data);
 
-        $('.event-group a').each(function () {
-            const eventAccommodation = $(this).find('.event-accommodation').text()
-            const eventName = $(this).find('.event-name').text()
-            const eventDate = $(this).find('.date-range').text() + $(this).find('.month').text()
-            const url = $(this).attr('href')
-            articles.push({
-                title: eventName,
-                eventDate,
-                eventAccommodation,
-                url
-            })
-        })
-        res.json(articles)
-    }).catch((err) => console.log(err))
-})
+    const articles = $(".event-group a").map(function () {
+      const eventAccommodation = $(this)
+        .find(".event-accommodation")
+        .text();
+      const eventName = $(this).find(".event-name").text();
+      const eventDate =
+        $(this).find(".date-range").text() + $(this).find(".month").text();
+      const url = $(this).attr("href");
 
-app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`))
+      return {
+        title: eventName,
+        eventDate,
+        eventAccommodation,
+        url,
+      };
+    }).get();
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
