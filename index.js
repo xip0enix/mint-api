7df84b2098687e1107b553fac6cf5134922b1568
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-05-05 16:55:06 +0200

    added node-cron and memory-cache to reduce loading

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const PORT = process.env.PORT || 7000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cache = require("memory-cache");
+const cron = require("node-cron");
 
 const app = express();
 
@@ -10,6 +12,50 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/veranstaltungen", async (req, res) => {
+  try {
+    const cacheKey = "veranstaltungen";
+
+    // Check if data exists in cache
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) {
+      return res.json(cachedData);
+    }
+
+    const response = await axios.get(
+      "https://www.mint-ec.de/angebote/veranstaltungen/kommende-veranstaltungen/"
+    );
+
+    const $ = cheerio.load(response.data);
+
+    const articles = $(".event-group a").map(function () {
+      const eventAccommodation = $(this)
+        .find(".event-accommodation")
+        .text();
+      const eventName = $(this).find(".event-name").text();
+      const eventDate =
+        $(this).find(".date-range").text() + $(this).find(".month").text();
+      const url = $(this).attr("href");
+
+      return {
+        title: eventName,
+        eventDate,
+        eventAccommodation,
+        url,
+      };
+    }).get();
+
+    // Cache the response data for 1 hour
+    cache.put(cacheKey, articles, 3600000);
+
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Schedule the task to run every hour
+cron.schedule("0 * * * *", async () => {
   try {
     const response = await axios.get(
       "https://www.mint-ec.de/angebote/veranstaltungen/kommende-veranstaltungen/"
@@ -34,10 +80,10 @@ app.get("/api/veranstaltungen", async (req, res) => {
       };
     }).get();
 
-    res.json(articles);
+    // Cache the response data for 1 hour
+    cache.put("veranstaltungen", articles, 3600000);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
